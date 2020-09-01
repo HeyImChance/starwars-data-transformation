c3a9468cb7c513200e40d66917ae124443b35de2
Chance Cooper <ccooper@flexera.com>
2020-09-01 09:36:24 -0400

    several changes needed during testing

--- task_two.py
+++ task_two.py
@@ -28,55 +28,57 @@ master_species_dict = {}
 # Take in json, extract all data that doesn't cross-reference
 def transform_char(character_dict):
     # Clean up cross-referencing data
-    for c in character_dict:
-        if (c['homeworld']):
-            del c['homeworld']
-        if (c['films']):
-            del c['films']
-        if (c['species']):
-            del c['species']
-        if (c['vehicles']):
-            del c['vehicles']
-        if (c['starship']):
-            del c['starship']
+    if 'homeworld' in character_dict:
+        del character_dict['homeworld']
+    if 'films' in character_dict:
+        del character_dict['films']
+    if 'species' in character_dict:
+        del character_dict['species']
+    if 'vehicles' in character_dict:
+        del character_dict['vehicles']
+    if 'starships' in character_dict:
+        del character_dict['starships']
+    if character_dict['height'] == 'unknown':
+        character_dict['height'] = 0
+    # Mass values are potentially in the 1000s
+    if ',' in character_dict['mass']:
+        character_dict['mass'] = character_dict['mass'].replace(',','')
+    if character_dict['mass'] == 'unknown':
+        character_dict['mass'] = 0
     convert_height(float(character_dict['height']))
     convert_weight(float(character_dict['mass']))
     return character_dict
 
 def transform_planet(planet_dict):
     # Clean up cross-referencing data
-    for p in planet_dict:
-        if (p['residents']):
-            del p['residents']
-        if (p['films']):
-            del p['films']
+    if 'residents' in planet_dict:
+        del planet_dict['residents']
+    if 'films' in planet_dict:
+        del planet_dict['films']
     return planet_dict
 
 def transform_starship(starship_dict):
     # Clean up cross-referencing data
-    for s in starship_dict:
-        if (s['pilots']):
-            del s['pilots']
-        if (s['films']):
-            del p['films']
+    if 'pilots' in starship_dict:
+        del starship_dict['pilots']
+    if 'films' in starship_dict:
+        del starship_dict['films']
     return starship_dict
 
 def transform_vehicle(vehicle_dict):
     # Clean up cross-referencing data
-    for v in vehicle_dict:
-        if (v['pilots']):
-            del v['pilots']
-        if (v['films']):
-            del v['films']
+    if 'pilots' in vehicle_dict:
+        del vehicle_dict['pilots']
+    if 'films' in vehicle_dict:
+        del vehicle_dict['films']
     return vehicle_dict
 
 def transform_species(species_dict):
     # Clean up cross-referencing data
-    for s in species_dict:
-        if (s['people']):
-            del s['people']
-        if (s['films']):
-            del s['films']
+    if 'people' in species_dict:
+        del species_dict['people']
+    if 'films' in species_dict:
+        del species_dict['films']
     return species_dict  
 
 def convert_height(height):
@@ -86,7 +88,6 @@ def convert_weight(weight):
     return (weight * 2.205)
 
 film_json = requests.get(f"http://swapi.dev/api/films/{film_num}/").json()
-film_dict = json.loads(film_json)
 
 characters_list = film_json.get('characters')
 planets_list = film_json.get('planets')
@@ -96,35 +97,30 @@ species_list = film_json.get('species')
 
 for character in characters_list:
     character_json = requests.get(character).json()
-    character_dict = json.loads(character_json)
-    master_character_dict.update(transform_char(character_dict))
+    master_character_dict.update(transform_char(character_json))
 
 for planet in planets_list:
     planet_json = requests.get(planet).json()
-    planet_dict = json.loads(planet_json)
-    master_planet_dict.update(transform_planet(planet_dict))
+    master_planets_dict.update(transform_planet(planet_json))
 
 for starship in starships_list:
     starship_json = requests.get(starship).json()
-    starship_dict = json.loads(starship_json)
-    master_starship_dict.update(transform_starship(starship_dict))
+    master_starships_dict.update(transform_starship(starship_json))
 
 for vehicle in vehicles_list:
     vehicle_json = requests.get(vehicle).json()
-    vehicle_dict = json.loads(vehicle_json)
-    master_vehicle_dict.update(transform_vehicle(vehicle_dict))
+    master_vehicles_dict.update(transform_vehicle(vehicle_json))
 
 for species in species_list:
     species_json = requests.get(species).json()
-    species_dict = json.loads(species_json)
-    master_species_dict.update(transform_species(species_dict))
+    master_species_dict.update(transform_species(species_json))
 
-film_dict['characters'] = master_character_dict
-film_dict['planets'] = master_planets_dict
-film_dict['starships'] = master_starships_dict
-film_dict['vehicles'] = master_vehicles_dict
-film_dict['species'] = master_species_dict
+film_json['characters'] = master_character_dict
+film_json['planets'] = master_planets_dict
+film_json['starships'] = master_starships_dict
+film_json['vehicles'] = master_vehicles_dict
+film_json['species'] = master_species_dict
 
-final_json = json.dumps(film_dict)
-with open('task_two.json', 'w') as outfile:
+final_json = json.dumps(film_json)
+with open('task_two.json', 'w', indent=4) as outfile:
     json.dump(final_json, outfile)
